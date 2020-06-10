7f7d8bd8c644fa8c240947de36b53f7eadf7ff62
zh <50803056+hhzule@users.noreply.github.com>
2020-06-10 17:01:57 +0500

    charts and selector done

--- src/api/index.js
+++ src/api/index.js
@@ -18,16 +18,33 @@ try {
 export const fetchDailyData = async () => {
     try {
         const { data} = await axios.get(`${url}/daily`);
+
       const modifiedData = data.map((dailyData)=>({
           confirmed: dailyData.confirmed.total,
           deaths : dailyData.deaths.total,
           date: dailyData.reportDate,
       }));
 
-      return modifiedData;
-       
+      return modifiedData; 
 
     } catch (error) {
+        console.log(error);
+        
         
     }
+}
+
+export const fetchCountries = async () => {
+
+try {
+    const {data : { countries } } = await axios.get(`${url}/countries`);
+    
+    return countries.map((country) => country.name);
+    
+
+
+} catch (error) {
+    console.log(error);
+    
+}
 }
